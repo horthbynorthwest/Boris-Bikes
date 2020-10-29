b0239ec5586c4023daa01d04607ceb3e1ae0650b
Georgie Horth <georgiehorth@gmail.com>
2020-10-29 16:11:02 +0000

    tests for docking station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,15 @@
 class DockingStation
+  @@bike = 0
+
   def release_bike
-    @bike = Bike.new
+    Bike.new
+  end
+  def dock_bike
+    if @@bike == 0
+      @@bike +=1
+      "Bike docked!"
+    elsif @@bike == 1
+      "No room for bikes"
+    end
   end
 end
--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -3,6 +3,7 @@ require 'docking_station'
 describe DockingStation do
   docking_station = described_class.new
   bike = docking_station.release_bike
+
   # responds to release_bike method"
     it { docking_station.should respond_to(:release_bike) }
 
@@ -15,5 +16,25 @@ describe DockingStation do
 
     it "checks the bike is working" do
       expect(bike.working?).to eq true
+    end
+
+    it { docking_station.should respond_to(:dock_bike) }
+
+    it "release_bike releases a bike" do
+      expect(docking_station.release_bike).to eq "Bike released!"
+    end
+
+    it "doesn't let you take a bike from an empty docking station" do
+      @bike == 0
+      expect(subject.release_bike).to eq ("No bikes available")
+    end
+
+    it "dock_bike docks bike" do
+      expect(docking_station.dock_bike).to eq "Bike docked!"
+    end
+
+    it "doesn't let you put a bike into a full docking station" do
+      @bike == 1
+      expect(subject.dock_bike).to eq ("No room for bikes")
     end
 end
